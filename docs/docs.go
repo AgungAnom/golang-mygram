6325a946d9e61623d5f4ee75a52b874a50d38229
I Gusti Agung Anom Semara Putra <agoenkanom1305@gmail.com>
2023-04-15 01:08:20 +0800

    finish railway

--- docs/docs.go
+++ docs/docs.go
@@ -646,7 +646,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:3000",
+	Host:             "https://golang-mygram-production-dfb5.up.railway.app/",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Golang MyGram API",
